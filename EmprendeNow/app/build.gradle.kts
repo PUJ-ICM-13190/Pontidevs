9df112fd0f3c083aaf35d13a9bc6f6fac08289ac
Renato Negrete <negreterenato@gmail.com>
2024-09-16 16:34:01 -0500

    pantalla inicial

--- EmprendeNow/app/build.gradle.kts
+++ EmprendeNow/app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.emprendenow"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.emprendenow"
         minSdk = 24
